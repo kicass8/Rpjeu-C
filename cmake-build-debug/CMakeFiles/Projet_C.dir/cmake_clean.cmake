a9087a3de628491c8e05232d94f2d89915230306
kicass8 <61785047+kicass8@users.noreply.github.com>
2021-10-11 18:44:52 +0200

    Added player structure and init, Added Monster structure and init, Added Items structure, init, addition into player and PNJ inventories, Added PNJ structure and init, Added thingsToRespawn structure (init and additon don't work atm), Added some printfs to tests the code.
    Added rpg.h with the prototypes of all our functions and structures.

---
+++ cmake-build-debug/CMakeFiles/Projet_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_C.dir/main.c.obj"
+  "Projet_C.exe"
+  "Projet_C.exe.manifest"
+  "Projet_C.lib"
+  "Projet_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
