85b32eaf66773598b7aa1a0598ec6602c0c8ff32
kicass8 <61785047+kicass8@users.noreply.github.com>
2021-10-11 18:43:52 +0200

    Added player structure and init, Added Monster structure and init, Added Items structure, init, addition into player and PNJ inventories, Added PNJ structure and init, Added thingsToRespawn structure (init and additon don't work atm), Added some printfs to tests the code.
    Added rpg.h with the prototypes of all our functions and structures.

---
+++ main.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rpg.h"
+
+//Returns a pointer to an Item after initialising it with the given parameters.
+Item* newItem(int id, int damage, int durability, int quantity, float protection, int heal){
+    Item* item = malloc(sizeof(Item));
+    item->id = id;
+    item->damage = damage;
+    item->durability = durability;
+    item->quantity = quantity;
+    item->protection = protection;
+    item->heal = heal;
+    return item;
+}
+
+//Returns a pointer to a Monster after initialising it with the given parameters.
+Monster* newMonster(char* name, int id, int HP, int attack, int expDrop){
+    if(id < 12 || id > 99){
+        return NULL;
+    }
+    Monster* monster = malloc(sizeof(Monster));
+    monster->name = name;
+    monster->id = id;
+    monster->HP = HP;
+    monster->attack = attack;
+    monster->expDrop = expDrop;
+    return monster;
+}
+
+//Returns a pointer to a PNJ after initialising it with an empty inventory.
+PNJ* newPNJ(){
+    PNJ* pnj = malloc(sizeof(PNJ));
+    Item* defaultInventory = malloc(sizeof(Item) * 255);
+    pnj->inventory = defaultInventory;
+    pnj->inventoryNextSpace = 0;
+    return pnj;
+}
+
+//Adds an already initialised Item to the Player's inventory.
+void addToPlayerInventory(Player* player, Item* item){
+    player->inventory[player->inventoryNextSpace] = *item;
+    player->inventoryNextSpace++;
+}
+
+//Adds an already initialised Item to a PNJ's inventory.
+void addToPNJInventory(PNJ* pnj, Item* item){
+    pnj->inventory[pnj->inventoryNextSpace] = *item;
+    pnj->inventoryNextSpace++;
+}
+
+//adds a thing to respawn to the respawn linked list
+thingToRespawn* addToRespawnList(thingToRespawn* head, int type, int x, int y, int map, int nbturns){
+    thingToRespawn* temp = head;
+    while (head->next != NULL){
+        head = head->next;
+    }
+    thingToRespawn* newThing = malloc(sizeof(thingToRespawn));
+    newThing->type = type;
+    newThing->x = x;
+    newThing->y = y;
+    newThing->map = map;
+    newThing->turnsUntilRespawn = nbturns;
+
+    head->next = newThing;
+    head = temp;
+    return head;
+}
+
+//To be called at the start of the game. Creates a Player with all the default values it needs.
+Player* initPlayer(){
+    Player* player = malloc(sizeof(Player));
+    Item* defaultInventory = malloc(sizeof(Item) * 10);
+    player->level = 1;
+    player->exp = 0;
+    player->expToNextLvl = 100;
+    player->HP = 100;
+    player->maxHP = 100;
+    player->inventory = defaultInventory;
+    player->inventoryNextSpace = 0;
+    int position[2] = {0, 0};
+    player->position = position;
+    addToPlayerInventory(player, newItem(1, 1, 10, -1, -1, -1));
+    addToPlayerInventory(player, newItem(2, -1, 10, -1, -1, -1));
+    addToPlayerInventory(player, newItem(3, -1, 10, -1, -1, -1));
+    addToPlayerInventory(player, newItem(4, -1, 10, -1, -1, -1));
+    return player;
+}
+
+int main() {
+    Player* player = initPlayer();
+
+    //Test to see if initPlayer and the other functions associated work
+    printf("Player's position : %d, %d\n", player->position[0], player->position[1]);
+    for (int i = 0; i < player->inventoryNextSpace; ++i) {
+        printf("Item id: %d, damage: %d, durability: %d\n", player->inventory[i].id, player->inventory[i].damage, player->inventory[i].durability);
+    }
+
+    //Test for monster creation
+    Monster* monster = newMonster("Dragon", 12, 25, 15, 100);
+    printf("Nom du monstre : %s, id : %d, HP : %d, attack : %d, expDrop : %d\n", monster->name, monster->id, monster->HP, monster->attack, monster->expDrop);
+
+    //Test for PNJ creation
+    PNJ* pnj1 = newPNJ();
+    addToPNJInventory(pnj1, newItem(1, 1, 10, -1, -1, -1));
+    for (int i = 0; i < pnj1->inventoryNextSpace; ++i) {
+        printf("PNJ1's inventory : id: %d, damage: %d, durability: %d\n", pnj1->inventory[i].id, pnj1->inventory[i].damage, pnj1->inventory[i].durability);
+    }
+
+    //Test for the linked list of things to respawn (NOT_WORKING)
+    thingToRespawn* thingsToRespawn = malloc(sizeof(thingToRespawn) * 100);
+    thingsToRespawn = addToRespawnList(thingsToRespawn, 3, 1, 1, 1,10);
+    while (thingsToRespawn->next != NULL){
+        printf("Thing -> type : %d, coordinates : {%d, %d} , map : %d, in how many turns : %d\n", thingsToRespawn->type, thingsToRespawn->x, thingsToRespawn->y, thingsToRespawn->map, thingsToRespawn->turnsUntilRespawn);
+        thingsToRespawn = thingsToRespawn->next;
+    }
+}
---
+++ rpg.h
@@ -0,0 +1,72 @@
+//
+// Created by kicas on 11/10/2021.
+//
+
+#ifndef PROJET_C_RPG_H
+#define PROJET_C_RPG_H
+
+//Item structure, all values should be either -1 (used to indicate that this item does not possess such characteristic) or a positive value. (if id == -1 then the item is a placeholder space)
+struct Item {
+    int id;
+    int damage;
+    int durability;
+    int quantity;
+    float protection;
+    int heal;
+};
+
+typedef struct Item Item;
+
+//Player structure, 'inventoryNextSpace' is used to keep track of the next available index of the inventory.
+struct Player {
+    int level;
+    int exp;
+    int expToNextLvl;
+    int HP;
+    int maxHP;
+    Item* inventory;
+    int inventoryNextSpace;
+    int* position;
+};
+
+typedef struct Player Player;
+
+//Monster structure, id must be comprised between 12 and 99
+struct Monster {
+    char* name;
+    int id;
+    int HP;
+    int attack;
+    int expDrop;
+};
+
+typedef struct Monster Monster;
+
+//PNJ structure, holds items, 'inventoryNextSpace' is used to keep track of the next available index of the inventory.
+struct PNJ {
+    Item* inventory;
+    int inventoryNextSpace;
+};
+
+typedef struct PNJ PNJ;
+
+//Linked list of things to respawn
+struct thingToRespawn {
+    int type;
+    int x;
+    int y;
+    int map;
+    int turnsUntilRespawn;
+    struct thingToRespawn* next;
+};
+
+typedef struct thingToRespawn thingToRespawn;
+
+thingToRespawn* addToRespawnList(thingToRespawn* list,int type, int x, int y, int map, int nbturns);
+PNJ* newPNJ();
+Monster* newMonster(char* name, int id, int HP, int attack, int expDrop);
+Item* newItem(int id, int damage, int durability, int quantity, float protection, int heal);
+void addToPlayerInventory(Player* player, Item* item);
+Player* initPlayer();
+
+#endif //PROJET_C_RPG_H
