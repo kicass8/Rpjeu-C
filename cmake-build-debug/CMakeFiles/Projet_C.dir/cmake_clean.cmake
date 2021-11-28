2a7cf757234e0de8bf7d4915b68dab1d8b8b376e
AlexandreMarcos94 <alexandremarcos94100@gmail.com>
2021-11-28 15:02:14 +0100

    small correction

--- cmake-build-debug/CMakeFiles/Projet_C.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Projet_C.dir/cmake_clean.cmake
@@ -1,6 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Projet_C.dir/main.c.o"
+  "CMakeFiles/Projet_C.dir/main.c.obj"
   "Projet_C.exe"
+  "Projet_C.exe.manifest"
   "Projet_C.pdb"
   "libProjet_C.dll.a"
 )
